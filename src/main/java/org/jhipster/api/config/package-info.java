9ea90dcdf9e05c97f16f00eb504520f414aa8f51
DarkMine <david76545@gmail.com>
2023-09-27 15:05:17 +0200

    Primo Commit

---
+++ src/main/java/org/jhipster/api/config/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Framework configuration files.
+ */
+package org.jhipster.api.config;
---
+++ src/main/java/org/jhipster/api/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package org.jhipster.api.repository;
+
+import org.jhipster.api.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
